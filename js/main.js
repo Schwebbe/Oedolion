ec3f8d9d8888ed34ea1a979dc69123eb1b670d8d
Sebastian Selin <Sebastian Selin>
2017-04-27 16:59:40 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+setTimeout(function(){
+    $('.cover').removeClass('cover').fadeOut(500);
+},10000);
+
+function move() {
+    var elem = document.getElementById("loadingBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100){
+            clearInterval(id);
+            
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}   
+
