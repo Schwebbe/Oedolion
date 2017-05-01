6f13d2a196e373db6ab394c9bed0094d860624bb
Schwebbe <sebbetheme@hotmail.com>
2017-05-02 01:54:05 +0200

    New goodies added!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-setTimeout(function(){
-    $('.cover').removeClass('cover').fadeOut(0);
-},14000);
-
+$(document).ready(function(){
+    $("#button").click(function(){
+        $(".cover").fadeOut(6000);
+    });
+});
+$(document).ready(function(){
+    $("#button").click(function(){
+        $(".stars").fadeIn(8000);
+    });
+});
+$(document).ready(function(){
+    $("#button").click(function(){
+        $(".twinkling").fadeIn(4000);
+    });
+});
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("#button").fadeOut(1000);
+    });
+});
+//moving loading bar animation
 function move() {
     var elem = document.getElementById("loadingBar");
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
         if (width >= 100){
+            //clears id when 100%
             clearInterval(id);
             
         } else {
